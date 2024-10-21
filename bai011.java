04fbb5c1343aa2ac48017386dd1a70b02ca60773
Đặng Tuấnn <datha.tuann79@gmail.com>
2024-10-21 14:39:50 +0700

    Update bai011.java
    line 14

--- bai011.java
+++ bai011.java
@@ -11,7 +11,7 @@ public class bai011 {
          int tong = a+b;
          int tich= a*b ;
          int hieu = a-b ;
-         double thuong = b != 0 ?( double ) a/b : Double.NaN;
+         double thuong = b != 0 ?( double ) a/b : Double.NaN;//dòng này tùm lùm zậy coi lại đi=))) b != 0 phải cho vào ngoặc nha 
         System.out.println( " tong "+tong );
         System.out.println( " tich "+tich );
         System.out.println( " hieu "+hieu );
